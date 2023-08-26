474d584f3463516465c80261eb8827337bd6cc6a
Halil ÖZCAN <mail.halilozcan@gmail.com>
2023-08-26 14:20:25 +0300

    add operator overloading

--- src/main/kotlin/25-OperatorOverloading.kt
+++ src/main/kotlin/25-OperatorOverloading.kt
@@ -4,6 +4,12 @@
  */
 
 /**
+ * x1 + x2	        x1.plus(x2)
+ * x1 – x2	        x1.minus(x2)
+ * x1 * x2	        x1.times(x2)
+ * x1/ x2	        x1.div(x2)
+ * x1 % x2	        x1.rem(x2)
+ * x1..x2	        x1.rangeTo(x2)
  * x1 in x2	        x2.contains(x1)
  * x1 !in x2	    !x2.contains(x1)
  * x[i]	            x.get(i)
@@ -13,8 +19,6 @@
  * x()	            x.invoke()
  * x(i)	            x.invoke(i)
  * x(i, j)	        x.invoke(i, j)
- * x1 + x2          x1.plus(x2)
- * x1 - x2          x1.minus(x2)
  * +x1              x1.unaryPlus()
  * -x1              x1.unaryMinus()
  * x1 += x2	        x1.plusAssign(x2)
