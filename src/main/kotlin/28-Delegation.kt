81b4757b209c4388981d641634765b64013db042
Halil ÖZCAN <mail.halilozcan@gmail.com>
2023-08-28 22:47:56 +0300

    add delegation

---
+++ src/main/kotlin/28-Delegation.kt
@@ -0,0 +1,89 @@
+/**
+ * Delegation bir nesnenin işini başka bir nesneye aktarmaktır. Örneğin sınıf B nin
+ * A ya verilerek A sınıfında kendisine ait bir şey çağırmasının yetkisinin verilmesine
+ * delegation denir.
+ */
+
+/**
+ * Delegation explicit ve implicit olmak üzere ikiye ayrılır.
+ */
+
+/**
+ * Explicit Delegation
+ */
+
+interface Destroyer {
+    fun destroy()
+}
+
+class SpecialRemover : Destroyer {
+    override fun destroy() {
+        println("SpecialRemover.destroy()")
+    }
+
+}
+
+open class UserView {
+    open fun show() {
+        println("View.Show()")
+    }
+}
+
+class Screen(private val view: UserView, private val destroyer: Destroyer) {
+    fun show() {
+        view.show()
+    }
+
+    fun destroy() {
+        destroyer.destroy()
+    }
+}
+
+class CustomView : UserView() {
+    override fun show() {
+        println("CustomView.show()")
+    }
+}
+
+/**
+ * Kotlin delegation ı işleminin implicit olarak yapılmasını sağlar. Class Delegation
+ * ve Delegated Properties feature larını sunar.
+ */
+
+interface Nameable {
+    var name: String
+}
+
+class Halil : Nameable {
+    override var name: String = "Halil"
+}
+
+class LongDistanceRunner : Runnable {
+    override fun run() {
+        println("run()")
+    }
+}
+
+/**
+ * Burada by ile delegation yapılmıştır. Yani verilen nameable a direkt olarak
+ * erişilebilir. Runnable ın içerisindeki fonksiyonu yazmadan direkt olarak
+ * erişilebilir.
+ */
+class PersonDelegation(name: Nameable, runner: Runnable) : Nameable by name, Runnable by runner
+
+fun main() {
+    /**
+     * Screen in show işleminin View nesnesine atanması işlemi delegationdır.
+     * Delegation işlemi aynı zamanda interfaceler aracılığıyla da yapılabilir.
+     */
+    val view = UserView()
+    val customView = CustomView()
+    val specialRemover = SpecialRemover()
+    val screen = Screen(customView, specialRemover)
+    screen.show()
+    screen.destroy()
+
+    val person = PersonDelegation(Halil(), LongDistanceRunner())
+    println(person.name)
+    println(person.run())
+}
