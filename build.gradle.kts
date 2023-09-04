e9f3be8d4b451d4f1f63b0cd4c7a1b2209c856f4
Halil ÖZCAN <mail.halilozcan@gmail.com>
2023-09-05 01:17:31 +0300

    add reflection (wip)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
     testImplementation(kotlin("test"))
 }
 
