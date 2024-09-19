decbb2d3c82e9c44c1db2fefe1a24a1458179154
Halil ÖZCAN <mail.halilozcan@gmail.com>
2024-09-19 22:45:30 +0300

    fix type check and casts

--- src/main/kotlin/10-TypeCheckAndCast.kt
+++ src/main/kotlin/10-TypeCheckAndCast.kt
@@ -1,53 +1,60 @@
-/**
-val x: String = ""
-
-if (x is String) {
-// Smart Cast
-x.length
-}
-
-if (x !is String) {
-println(x)
-}
-
-if(x !is String) return
-// Smart Cast obj.length
-
-
-// x is automatically cast to String on the right-hand side of `||`
-if (x is String || x.length == 0) return
-
-// x is automatically cast to String on the right-hand side of `&&`
-if (x is String && x.length > 0) {
-print(x.length) // x is automatically cast to String
-}
-
-// Smart Cast
-when (x) {
-is Int -> print(x + 1)
-is String -> print(x.length + 1)
-is IntArray -> print(x.sum())
-}
-
-// SmartCast Koşulları
-val lokal değişkenler - her zaman smart cast olabilirler (delegated properties haricinde)
-val propertyler -> property private sa ya da internalsa veya check işlemi aynı module içerisinde ypaılıyorsa veya custom
-getter yoksa smart cast yapılabilir
-var lokal değişkenler -> kullanım ve kontrol aşamasında değiştirilme yoksa veya kendisini değiştiren lambda yoksa ve local
-delegated property değilse smart cast yapılabilir
-var property -> asla yapılmaz çünkü her zaman değiştirilebilirler.
- */
-
-/**
- *  // Unsafe Cast
- * // Null nullable olmayan değere çevrilemez
- *  val x:String = y as String
- *  val x:String? = y as String?
- *
- *  // Safe Cast
- *  val x:String? = y as? String
- */
+package org.example
 
 fun main() {
+    val x = getValue()
+    if (x is String) {
+        println(x.length)
+    }
 
+    if (x !is String) {
+
+    }
+
+    processX(x)
+
+    /** otomatik cast işlemi yapılmaz
+    if(x is String || x.length){
+
+    }
+     **/
+
+    // otomatik olarak cast eder
+    if (x is String && x.length > 0) {
+
+    }
+
+    when (x) {
+        is Int -> println(x + 1)
+        is String -> println(x.length)
+        is IntArray -> println(x.sum())
+    }
+
+    val y: String? = null
+    // Null, nullable olmayan değere çevrilemez. Bu işlem unsafe bir cast işlemidir
+    // val anotherX: String = y as String
+    // val anotherXCast:String = y as String?
+
+    val safeX: String? = y as? String
+
+    /**
+     * Smartcast Koşulları
+     * val local değişkenler -> her zaman smart cast olabilirler (delegated properties haricinde)
+     * val propertyler -> property private ya da internalsa veya check işlemi aynı module içerisinde yapılıyorsa
+     * veya custom getter yoksa smart cast yapılabilir.
+     * var local değişkenler -> kullanım ve kontrol aşamasında değiştirilme yoksa veya kendini değiştiren bir lambda
+     * yoksa ve local delegated property değilse smart cast yapılabilir.
+     * var property -> asla yapılmaz, çünkü her zaman değiştirilebilirler
+     */
+}
+
+fun getValue(): Any {
+    return "Halil"
+}
+
+fun processX(x: Any) {
+    if (x !is String) {
+        return
+    }
+
+    println(x.length)
 }
